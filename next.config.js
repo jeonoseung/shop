8eae32828741088c069354cf2240d86957ac1b30
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-02-17 18:09:15 +0900

    폰트 파일 변경 otf -> woff2로 변경

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: false,
+//   swcMinify: true,
+//   experimental: {
+//     scrollRestoration: true,
+//   },
+// }
+// module.exports = nextConfig
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
   reactStrictMode: false,
   swcMinify: true,
   experimental: {
     scrollRestoration: true,
   },
-}
-module.exports = nextConfig
+});
