c78f1116689abaa607fdc65ee4817c194697b2f6
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-02-20 00:46:59 +0900

    기능 수정
    뒤로 가기 시 캐시 삭제되는 현상 수정
    스크롤 복구를 위해서 수정함

--- pages/admin/product/list.tsx
+++ pages/admin/product/list.tsx
@@ -4,7 +4,7 @@ import {useInfiniteQuery} from "react-query";
 import {ProductListType} from "product-type";
 import axios from "axios";
 import Spinner from "../../../src/component/public/spinner";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import ProductListManagementOption from "../../../src/component/product/admin/list/list-option";
 import ProductManagementList from "../../../src/component/product/admin/list/product-li";
@@ -12,6 +12,7 @@ import SetInViewProductManagement from "../../../src/component/product/admin/lis
 
 export default function ProductListManagement(){
     const router = useRouter();
+    const [ready,setReady] = useState(false)
     const {data,error,isLoading,fetchNextPage,hasNextPage,refetch,remove} =
         useInfiniteQuery('product-li',({pageParam=1})=>fetchProjects(pageParam),{
         getNextPageParam:(lastPage)=>lastPage.nextPage
@@ -23,9 +24,12 @@ export default function ProductListManagement(){
         return {products:res.data,nextPage:res.data.length < 10 ? undefined : pageParam+1}
     }
     useEffect(()=>{
-        if(!isLoading){
+        if(!isLoading && ready){
             remove()
             refetch()
+        }
+        else{
+            setReady(true)
         }
     },[router.query.search])
     return(
