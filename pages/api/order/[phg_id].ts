696d4e9966f93e2c737aa9e6d072ca93279719e4
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-02-20 14:17:46 +0900

    상품 삭제 시 주문 리스트 버그 수정
    주문 내역에 존재하는 상품이 삭제 될 시 주문 내역에서 삭제된 상품 알림 표시 기능 추가

--- pages/api/order/[phg_id].ts
+++ pages/api/order/[phg_id].ts
@@ -9,7 +9,7 @@ export const get = async (req:NextApiRequest,res:NextApiResponse) =>{
         const user = req.query.user ? req.query.user : req.session.user.id;
         const sql = `SELECT ph.product_id,p.product_name,p.brand_name,p.product_img,p.product_price,p.discount_rate,ph.count,ph.phg_id
                 FROM purchase_history AS ph
-                INNER JOIN products as p ON p.product_id = ph.product_id
+                LEFT JOIN products as p ON p.product_id = ph.product_id
                 WHERE ph.phg_id = ${req.query.phg_id}
                 AND ph.user_id = ${user}`
         const [rows] = await connection.query(sql)
--- src/component/my-page/order-detail-list.tsx
+++ src/component/my-page/order-detail-list.tsx
@@ -1,7 +1,4 @@
 import styles from "./my-page.module.css";
-import Spinner from "../public/spinner";
-import {setDateOnOrderList} from "../../function/public/order-list-date";
-import Link from "next/link";
 import Image from "next/image";
 import {setPrice, totalPrice} from "../../function/public/price";
 import publicStyles from "../../../styles/public.module.css";
@@ -9,16 +6,14 @@ import {useRouter} from "next/router";
 import {useQuery} from "react-query";
 import {getOrderDetail} from "../../function/api/get/api";
 import setProductName from "../../function/public/product-name";
-import {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../../store/store";
+import {useDispatch} from "react-redux";
 import {setDisplay, setProductInfo} from "../../../store/modal/cart-modal";
 
 export default function OrderDetailList(){
     const route = useRouter()
     const {data} = useQuery('order-detail-li',()=>getOrderDetail(false,route.query.phg_id as string))
     const dispatch = useDispatch()
-    const modal = useSelector((state:RootState)=>state.cartModal)
+    console.log(data)
     return(
         <div className={styles['pages-list']}>
             <div className={styles['title-div']}>
@@ -31,25 +26,35 @@ export default function OrderDetailList(){
                 {
                     data.map((li:any)=>(
                         <div className={styles['order-detail-list']} key={li.product_id}>
-                            <Image src={li.product_img} alt={'상품 이미지'} width={60} height={85} priority={true}/>
+                            <Image src={li.product_img ? li.product_img : '/image/null-image.svg'} alt={'상품 이미지'} width={60} height={85} priority={true}/>
                             <div className={styles['info']}>
                                 <div>
-                                    <span className={styles['name']}>{setProductName(li.brand_name,li.product_name)}</span>
+                                    <span className={styles['name']}>{li.product_name ? setProductName(li.brand_name,li.product_name) : '삭제된 상품입니다'}</span>
                                 </div>
-                                <div className={styles['price-count']}>
-                                    <span className={styles['price']}>{setPrice(totalPrice(li.product_price,li.discount_rate))}원</span>
-                                    {li.discount_rate !== 0 ? <span className={styles['line-through']}>{li.product_price}원</span> : null}
-                                    <span className={publicStyles['sign_or']}>|</span>
-                                    <span>{li.count}개</span>
-                                </div>
+                                {
+                                    li.product_price
+                                        ?
+                                        <div className={styles['price-count']}>
+                                            <span className={styles['price']}>{setPrice(totalPrice(li.product_price,li.discount_rate))}원</span>
+                                            {li.discount_rate !== 0 ? <span className={styles['line-through']}>{li.product_price}원</span> : null}
+                                            <span className={publicStyles['sign_or']}>|</span>
+                                            <span>{li.count}개</span>
+                                        </div>
+                                        : <div>-</div>
+                                }
                             </div>
                             <div>
-                                <button className={publicStyles['button']} onClick={()=>{
-                                    dispatch(setDisplay(true))
-                                    dispatch(setProductInfo({id:li.product_id,name:li.product_name,brand:li.brand_name,price:li.product_price,discount:li.discount_rate}))
-                                }}>
-                                    장바구니 담기
-                                </button>
+                                {
+                                    li.product_name
+                                        ?
+                                        <button className={publicStyles['button']} onClick={()=>{
+                                            dispatch(setDisplay(true))
+                                            dispatch(setProductInfo({id:li.product_id,name:li.product_name,brand:li.brand_name,price:li.product_price,discount:li.discount_rate}))
+                                        }}>
+                                            장바구니 담기
+                                        </button>
+                                        : null
+                                }
                             </div>
                         </div>
                     ))
