77dbc774bfe6dcb0bd3b3f21ae1ff8dc25c35a1c
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-02-26 00:07:04 +0900

    이미지 슬라이더 수정
    끝 이미지에서 다음 이미지로 넘어갈때 애니메이션 없이 넘어가는 현상 수정
    setTimeout으로 주요 코드들 실행되는 순서 변경

--- src/component/home/image-slider/image-slider.tsx
+++ src/component/home/image-slider/image-slider.tsx
@@ -1,61 +1,67 @@
 import {CSSProperties, useState, useRef, useEffect} from "react";
-import Image from "next/image";
 import styles from './image-slider.module.css'
 import LeftIcon from "../../public/icon/left-icon";
 import RightIcon from "../../public/icon/right-icon";
 
 export default function ImageSlider({images}:{images:{src:string}[]}){
+    const ms = 300;
     const [hover,setHover] = useState<boolean>(false)
     const nextButton = useRef<HTMLButtonElement>(null);
     const length = images.length+1
     const [Index,setIndex] = useState<number>(0)
     const [overIndex,setOverIndex] = useState<number>(0)
     const [sliderCSS, setSliderCSS] = useState<CSSProperties>({
-        transitionDuration:`0s`,
+        transitionDuration:`0ms`,
     })
     const previousImage = () =>{
-        if(sliderCSS.transitionDuration !== '0s') return;
+        if(sliderCSS.transitionDuration !== '0ms') return;
         const current = Index - 1;
         if(current < 0)
         {
-            setSliderCSS({...sliderCSS,transitionDuration:`0s`,transform:`translate3d(-${(length-1) * 100}vw,0,0)`})
+            setSliderCSS({...sliderCSS,transitionDuration:`0ms`,transform:`translate3d(-${(length-1) * 100}vw,0,0)`})
             setOverIndex(overIndex < 0 ? overIndex-1 : -1)
         }
         else
         {
-            setSliderCSS({...sliderCSS, transitionDuration:`0.3s`, transform:`translate3d(-${current * 100}vw,0,0)`})
+            setSliderCSS({...sliderCSS, transitionDuration:`${ms}ms`, transform:`translate3d(-${current * 100}vw,0,0)`})
         }
         setIndex(current < 0 ? length-1 : current)
     }
     const nextImage = () =>{
-        if(sliderCSS.transitionDuration !== '0s') return
+        if(sliderCSS.transitionDuration !== '0ms') return;
         const current = Index + 1;
         const value = Index === length-1 ? 0 : current
         if(Index === length-1)
         {
-            setSliderCSS({...sliderCSS,transitionDuration:`0s`,transform:`translate3d(0vw,0,0)`})
+            setSliderCSS({...sliderCSS,transitionDuration:`0ms`,transform:`translate3d(0vw,0,0)`})
             setOverIndex(overIndex >= 1 ? overIndex+1 : 1)
         }
         else
         {
-            setSliderCSS({...sliderCSS,transitionDuration:`0.3s`,transform:`translate3d(-${current * 100}vw,0,0)`})
+            setSliderCSS({...sliderCSS,transitionDuration:`${ms}ms`,transform:`translate3d(-${current * 100}vw,0,0)`})
         }
         setIndex(value)
     }
     const time:any = useRef();
     useEffect(()=>{
-        setTimeout(()=>setSliderCSS({...sliderCSS,transitionDuration:'0s'}),300)
         time.current = setInterval(()=>nextButton.current?.click(),10000)
         return ()=>{
             clearInterval(time.current)
         }
     },[Index])
     useEffect(()=>{
-        overIndex < 0
-            ? previousImage()
-            : overIndex > 0
-                ? nextImage()
-                : null
+        const {transitionDuration} = sliderCSS;
+        if(transitionDuration !== '0ms'){
+            setTimeout(()=>setSliderCSS({...sliderCSS,transitionDuration:'0ms'}),1)
+        }
+    },[sliderCSS])
+    useEffect(()=>{
+        if(overIndex < 0){
+            setTimeout(()=>previousImage(),50)
+        }
+        else if(overIndex > 0){
+            setTimeout(()=>nextImage(),50)
+        }
     },[overIndex])
     return(
         <div className={styles['image-slider']} onMouseOver={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
