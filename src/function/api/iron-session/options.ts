5552774e32afbdbd0696a4bfdb2d45607ca8a04e
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-05-14 20:34:47 +0900

    Fix: [수정] 세션 옵션 재변경

--- src/function/api/iron-session/options.ts
+++ src/function/api/iron-session/options.ts
@@ -1,10 +1,12 @@
-export const IronSessionOption = {
+import {IronSessionOptions} from "iron-session";
+
+export const IronSessionOption:IronSessionOptions = {
     cookieName: "cookie_cookie",
     password: process.env.SESSION_COOKIE ? process.env.SESSION_COOKIE : '',
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
         secure: false,
-        sameSite: true,
+        sameSite: "strict",
         maxAge:60 * 60000,
         httpOnly: false
     }
