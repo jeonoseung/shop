f2340c7911fcc2509933c996e6dc0da5fc7d9498
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-05-14 19:58:32 +0900

    Fix: [수정] 세션 cookie 옵션

--- src/function/api/iron-session/options.ts
+++ src/function/api/iron-session/options.ts
@@ -3,7 +3,9 @@ export const IronSessionOption = {
     password: process.env.SESSION_COOKIE ? process.env.SESSION_COOKIE : '',
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
+        sameSite: "Strict",
         maxAge:60 * 60000,
+        httpOnly: false
     }
 }
